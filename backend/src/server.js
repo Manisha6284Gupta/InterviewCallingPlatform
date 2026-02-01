c53813ddbb9963d720e59b9484e3a506c50530f9
Manisha6284Gupta <mg62849036@gmail.com>
2026-02-01 23:25:25 +0530

    mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,26 @@
 import express from "express";
 import {ENV} from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 const app = express()
 
-console.log(ENV.PORT)
+// console.log(ENV.PORT)
 // console.log(ENV.DB_URL)
 
 app.get("/", (req,res) => {
     res.status(200).json({meg:"success from api"})
 })
 
-app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT))
+
+
+const startServer = async () => {
+    try{
+        await connectDB();
+        app.listen(ENV.PORT, () => {console.log("Server is running on port:", ENV.PORT)
+       
+})
+    }catch(error){
+      console.log("Error starting the server",error)
+    }
+}
+
+startServer();
